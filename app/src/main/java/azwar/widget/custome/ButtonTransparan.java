6b731a6d17e25bb25d569ca1a9f65990405e65c4
azwar <azwarhalimu@gmail.com>
2022-12-12 22:47:19 +0800

    FINISH NNTI ZAIN YANG SELESAIKAN

---
+++ app/src/main/java/azwar/widget/custome/ButtonTransparan.java
@@ -0,0 +1,27 @@
+package azwar.widget.custome;
+
+import static android.R.attr.textColor;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class ButtonTransparan  extends androidx.appcompat.widget.AppCompatButton {
+
+    public ButtonTransparan(@NonNull Context context) {
+        super(context);
+    }
+
+    public ButtonTransparan(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    public ButtonTransparan(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
